a45d47ccd865215874d9a0d0e1ae66551d6a2fb9
sjo <oriol.plazas.7e7@itb.cat>
2025-05-12 19:45:09 +0200

    test carrito

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -2,34 +2,24 @@ const emojiUsuario = "\u{1F464}"; // Código Unicode para 👤
 
 // Esperamos a que todo el contenido del documento se haya cargado antes de ejecutar el código.
 document.addEventListener("DOMContentLoaded", function () {
-    // Se obtiene del almacenamiento local la información sobre si el usuario está logueado.
     const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
     const username = localStorage.getItem('username');
 
-    // Se selecciona el elemento donde se mostrará la información del usuario.
     const loginInfoElement = document.getElementById('login-info');
-
-    // Se selecciona el formulario con el id 'venta-form'.
     const ventaFormElement = document.getElementById('venta-form');
 
     console.log("Estado de sesión:", isLoggedIn);
     console.log("Usuario:", username);
-    console.log("Elemento login-info:", loginInfoElement);
-    console.log("Elemento venta-form:", ventaFormElement);
 
-    if (loginInfoElement && ventaFormElement) {
-        if (isLoggedIn && username) {
-            console.log("Usuario logueado, actualizando la interfaz...");
-            loginInfoElement.textContent = `${emojiUsuario} ${username}`;
-            loginInfoElement.classList.add("usuario-activo");
-            ventaFormElement.style.display = 'block'; // Muestra el formulario
-        } else {
-            console.log("No hay usuario logueado, restaurando el mensaje...");
-            loginInfoElement.innerHTML = 'Inicia sesión <a class="login-link" href="./login.html">aquí</a> para poder comprar';
-            loginInfoElement.classList.remove("usuario-activo");
-            ventaFormElement.style.display = 'none'; // Oculta el formulario
-        }
+    if (isLoggedIn && username) {
+        console.log("Usuario logueado, mostrando el formulario...");
+        loginInfoElement.textContent = `${emojiUsuario} ${username}`;
+        loginInfoElement.classList.add("usuario-activo");
+        ventaFormElement.style.display = 'block';
     } else {
-        console.log("Error: No se encontraron los elementos en el DOM.");
+        console.log("No hay sesión activa, ocultando formulario...");
+        loginInfoElement.innerHTML = 'Inicia sesión <a class="login-link" href="./login.html">aquí</a> para poder comprar';
+        loginInfoElement.classList.remove("usuario-activo");
+        ventaFormElement.style.display = 'none';
     }
 });

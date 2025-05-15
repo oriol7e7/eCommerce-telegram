30c4770e60bd2a014add91efa35e28899175f6c6
oriioll <oriolplazas2@gmail.com>
2025-05-15 22:38:35 +0200

    Create cancel.php

---
+++ cancel.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Compra Cancelada</title>
+</head>
+<body>
+    <h1>Compra cancelada</h1>
+    <p>Puedes volver a intentarlo cuando quieras.</p>
+    <a href="/">Volver a la tienda</a>
+</body>
+</html>
